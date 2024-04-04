f6fe6728ba4bd3838ab4d94c69e7cd75a3f0bf15
Sutirtho Paul <sutirthopaul100@gmail.com>
2024-04-04 10:48:20 +0530

    1st commit after stash

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 class Main {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
 
+        System.out.println("Hello, there!");
         System.out.println("Hello, World!");
         System.out.println("Hello, World1!");
         System.out.println("Hello, World2!");
         System.out.println("Hello, World3!");
-
-    }
 }
