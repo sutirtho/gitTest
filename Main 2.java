fabab2cefe407c93596d784eb67144eb4e641dc6
Sutirtho Paul <sutirthopaul100@gmail.com>
2024-10-18 21:23:47 +0530

    g1

--- Main 2.java
+++ Main 2.java
@@ -4,22 +4,4 @@ class Main {
         System.out.println("Hello, there!");
 
 }
-
-public void newMethod()
-{
-System.out.println("Hello, This is a newly added method!");
-	}
-
-<<<<<<< Updated upstream
-public void newMethod()
-{
-System.out.println("Hello, This is a newly added method2!");
-	}
-
-public void newMethod3()
-{
-System.out.println("Hello, This is a newly added method3!");
-	}
-=======
->>>>>>> Stashed changes
 }
